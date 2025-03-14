ee249d1688a8e8e17e7e8677af46aff56fefd3d8
chenhao <hao.chen01@boway.com>
2025-03-14 11:50:26 +0800

    添加代码序号，添加代码目录以及TODOs

---
+++ 3.langgraph/09.langgraph_graph_run.py
@@ -0,0 +1,62 @@
+from typing_extensions import Annotated, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_community.tools import DuckDuckGoSearchRun
+from langgraph.prebuilt import ToolNode, tools_condition
+from model_init import initialize
+llm = initialize()
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, ..., add_messages]
+
+
+
+graph_builder = StateGraph(State)
+
+tool = DuckDuckGoSearchRun()
+tools = [tool]
+llm_with_tools = llm.bind_tools(tools)
+
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.set_entry_point("chatbot")
+graph = graph_builder.compile()
+
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            # print(value)
+            print("Assistant:", value["messages"][-1].content)
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
--- Readme.md
+++ Readme.md
@@ -1,1 +1,27 @@
 # 记录langchain学习所用的code
+个人学习所用，代码多来自官网示例，会持续更新（LangChain和LangGraph的代码都合并在`3.langgraph`里
+
+# TODOs:
+- [x] LangChain核心内容
+- [ ] 添加LangChain主要数据结构用法(Runnable)
+- [ ] 添加LangGraph示例
+- [ ] 丰富代码注释
+- [ ] OpenManus核心代码剖析
+  
+# 代码目录
+|代码|备注|
+|---|---|
+|[1.rag](1.rag/rag_pipeline.py)|RAG的简单实现，使用pdfminer解析pdf，按字符split chunk，用Qwen/Qwen2.5-0.5B-Instruct模型，embedding模型是bge-base-zh-v1.5|
+|[2.pydantic](2.pydantic/demo.py)|pydantic的BaseModel类的初步了解，主要了解BaseModel的json schema，在后续工具调用和json解析会使用|
+|[3.langgraph/model_init.py](3.langgraph/model_init.py)|初始化llm，包括GLM-4-flash和GLM-4V-flash|
+|[3.langgraph/01.messages.py](3.langgraph/01.messages.py)|langchain的基本Message类，包括SystemMessage、HumanMessage、AIMessage|
+|[3.langgraph/02.multimodal.py](3.langgraph/02.multimodal.py)|多模态模型的调用content形式|
+|[3.langgraph/03.langchain_basis.py](3.langgraph/03.langchain_basis.py)|langchain实现简单QA chain的示例|
+|[3.langgraph/04.tool_demo.py](3.langgraph/04.tool_demo.py)|内置tool的使用和@tool装饰器|
+|[3.langgraph/05.tool_concept.py](3.langgraph/05.tool_concept.py)|tool的基本属性以及调用|
+|[3.langgraph/06.tool_calling.py](3.langgraph/06.tool_calling.py)|使用tool和不适用tool的结果对比|
+|[3.langgraph/07.structured_outputs.py](3.langgraph/07.structured_outputs.py)|输出结构化，json形式|
+|[3.langgraph/08.stream.py](3.langgraph/08.stream.py)|流式输出，同步流式，异步流式，事件流|
+|[3.langgraph/09.langgraph_graph.ipynb](3.langgraph/09.langgraph_graph.ipynb)|langgraph的基本对话Graph构建流程，以及如何在graph中加入tool调用|
+|[3.langgraph/09.langgraph_graph_run.py](3.langgraph/09.langgraph_graph_run.py)|langgraph的基本对话Graph，带tool调用|
+|[3.langgraph/10.memory_langgraph.ipynb](3.langgraph/10.memory_langgraph.ipynb)|带有Memory的Graph，带tool调用|
